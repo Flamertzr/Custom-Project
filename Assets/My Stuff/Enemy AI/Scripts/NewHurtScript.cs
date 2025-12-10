be794c8bf80c21ec41ce8d573373b1ea67cdad50
Flamertzr <mtvanarsdale2008@gmail.com>
2025-12-09 21:43:34 -0500

    Stuff

--- Assets/My Stuff/Enemy AI/Scripts/NewHurtScript.cs
+++ Assets/My Stuff/Enemy AI/Scripts/NewHurtScript.cs
@@ -5,13 +5,17 @@ public class enemyHurtScript : MonoBehaviour
     [SerializeField] public float iframes;
     [SerializeField] public float iframesDuration;
     [SerializeField] public int health = 100;
+
     public bool dead = false;
 
+    private GameObject enemy;
+    private Rigidbody2D body;
     private BoxCollider2D boxCollider;
     private SpriteRenderer sprite;
     private Color originalColor;
     private GameObject player;
     private PlayerAttackScript playerAttack;
+    private Animator anim;
     
 
     
@@ -22,19 +26,30 @@ public class enemyHurtScript : MonoBehaviour
     void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
+        body = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         player = GameObject.Find("Player");
+        enemy = gameObject;
         playerAttack = player.GetComponent<PlayerAttackScript>();
         originalColor = sprite.color;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
         iframes -= Time.deltaTime;
         if (health <=0)
         {
             dead = true;
+            anim.SetBool("Dead", true);
+            boxCollider.enabled = false;
+            body.simulated = false;
+            if (stateInfo.IsName("Death Animation") && stateInfo.normalizedTime >= 1.0f )
+            {
+                Destroy(gameObject);
+            }
         }
     }
 
@@ -50,9 +65,8 @@ public class enemyHurtScript : MonoBehaviour
 
     private System.Collections.IEnumerator hits()
     {
-        sprite.color = Color.red;        
-        yield return new WaitForSeconds(.2f);
-        sprite.color = originalColor;  
-        yield return new WaitForSeconds(iframesDuration);  
+            sprite.color = Color.red;        
+            yield return new WaitForSeconds(.5f);
+            sprite.color = originalColor;
     }
 }
--- Assets/My Stuff/Enemy AI/Scripts/followScript.cs
+++ Assets/My Stuff/Enemy AI/Scripts/followScript.cs
@@ -18,17 +18,23 @@ public class followScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+   void Update()
     {
-        if (hurtScript.dead != true)
+        if (hurtScript.dead == false)
         {
             Vector2 direction = player.transform.position - transform.position;
 
-            
-
+            if (direction.x > 0f)
+            {
+                transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+            } else if (direction.x < 0f) 
+            {
+                transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+            }
+            distance = Vector2.Distance(transform.position, player.transform.position);
             Vector2 targetPos = new Vector2(player.transform.position.x, transform.position.y);
-
             transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
         }
+        
     }
 }
