605e4c0aa6c623a2ffbc971e6b6f3e81903e784a
MatthewVanArsdale <mtvanarsdale2008@gmail.com>
2026-03-02 15:13:04 -0500

    L1 Update + Bandit enemy

---
+++ Assets/My Stuff/Enemy AI/Scripts/Bandit Scripts/banditAttackScript.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class banditAttackScript : MonoBehaviour
+{
+    [SerializeField] public float flinchDuration;
+    private float close;
+    private Animator anim;
+    private banditFollowScript follow;
+    private banditHurtScript HurtScript;
+    
+
+    private static float punchCooldown = 3f;
+    private static float punchTimer = 0;
+
+    public float hurtTimer;
+
+    public int punchDmg = 5;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        follow = GetComponent<banditFollowScript>();
+        HurtScript = GetComponent<banditHurtScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       hurtTimer -= Time.deltaTime;
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+        close = Vector2.Distance(transform.position, follow.player.transform.position);
+        punchTimer -= punchCooldown;
+
+        if (close > 40)
+        {
+            anim.SetBool("Punch", false);
+        } else 
+        {
+            if (punchTimer <= 0 && close <= 40 && hurtTimer < 0)
+            {
+                anim.SetBool("Punch", true);
+            }
+        } 
+    }
+}
