4e6932a409b8a00da2370aca55b92ec235abbc78
MatthewVanArsdale <mtvanarsdale2008@gmail.com>
2026-03-10 15:05:31 -0400

    Bandit Cutscene Setup

--- Assets/My Stuff/Other Scripts/Cutscene Scripts/banditCutsceneScript.cs
+++ Assets/My Stuff/Other Scripts/Cutscene Scripts/banditCutsceneScript.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class banditCutsceneScript : MonoBehaviour
 {
+
+    [SerializeField] private GameObject banditTextStuff; 
+    int cutsceneActivated = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        banditTextStuff.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            cutsceneActivated += 1;
+            if (cutsceneActivated == 1)
+            {
+                banditTextStuff.SetActive(true);
+            }
+        }
+
     }
 }
